0bdb6c80bcb83046ed0929dce6c0ee582e5c69a6
Michael-AngelCevallos <118484898+Michael-AngelCevallos@users.noreply.github.com>
2026-04-12 06:44:14 -0500

    setting up practice for variables

--- day1_variables.py
+++ day1_variables.py
@@ -1,3 +1,39 @@
 # Python PHASE  1 - DAY 1- VARIABLES
 
 print("Test")
+
+# Variables - What is a variable? A variable - A CONTAINER THAT STORES VALUE. It is a way to store data in a program. A variable can be assigned a value and that value can be changed later in the program.
+
+# Example of a variable
+name = "Michael"
+print(name)
+
+# Variable types - There are different types of variables in Python. The most common types are:
+# 1. String(STORE TEXT) - A string is a sequence of characters. It is used to store text. It is defined by enclosing the characters in quotes. For example:
+name2 = "Mike1"
+print(name2)
+
+# ! Variable Types - 4 types - 
+ # ?
+  # ? 1. STRING - USED TO HOLD TEXT\WORDS\SENTENCES - DEFINED BY ENCLOSING THE CHARACTERS IN QUOTES - EXAMPLE: name = "Michael"
+  # ? 2. INTEGER - USED TO HOLD WHOLE NUMBERS - DEFINED BY ASSIGNING A WHOLE NUMBER TO A VARIABLE - EXAMPLE: age = 30
+  # ? 3. FLOAT - USED TO HOLD DECIMAL NUMBERS - DEFINED BY ASSIGNING A DECIMAL NUMBER TO A VARIABLE - EXAMPLE: price = 19.99
+  # ? 4. BOOLEAN - USED TO HOLD TRUE OR FALSE VALUES - DEFINED BY ASSIGNING TRUE OR FALSE TO A VARIABLE - EXAMPLE: is_student = True
+
+#   pRACTICE -
+# Declare and print -
+name3 = "Sarah"
+print(name3)
+
+# ! INTEGER -
+age = 25
+print(age)
+
+# ! FLOAT - 
+price = 19.99
+print(price)
+
+# ! BOOLEAN -
+is_student = True
+print(is_student)
+
---
+++ variable_practice.py
@@ -0,0 +1,10 @@
+
+
+name = "Barabass"
+age = 21
+city = "New York"
+
+print(name)
+print(age)
+print(city)
+
