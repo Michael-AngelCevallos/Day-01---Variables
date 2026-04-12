690acdeb2f8326847744ed01ce6938f1335d2508
Michael-AngelCevallos <118484898+Michael-AngelCevallos@users.noreply.github.com>
2026-04-12 07:15:46 -0500

    test

--- variable_practice.py
+++ variable_practice.py
@@ -1,6 +1,6 @@
 
 
-name = "Barabass"
+name = "Jay"
 age = 21
 city = "New York"
 
@@ -8,3 +8,8 @@ print(name)
 print(age)
 print(city)
 
+# ! Use variables in sentence-
+
+print("My name is " + name + " and I am " + str(age) + " years old. I live in " + city + ".")
+
+print("My name is {} and I am {} years old. I live in {}.".format(name, age, city))      
